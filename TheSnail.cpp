3e5fb9ce5bf005adec9f8ed8e95973a7b3e41cd7
Michael <mickconnors2014@gmail.com>
2019-08-19 13:07:32 +0100

    Create TheSnail.cpp

---
+++ TheSnail.cpp
@@ -0,0 +1,40 @@
+//19/08/19
+/*
+UVA problem 539
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int H, U, D, F;
+	while(scanf("%d %d %d %d", &H, &U, &D, &F) && H != 0){
+		int day=1;
+		double climb =(double)U;
+		double progress=0;
+		double fatigue=(double)F/100;
+		double loss=fatigue*climb;
+		while(true){
+			progress += climb;
+			if(progress > H){
+				printf("success on day %d\n", day);
+				break;
+			}
+			progress -= D;
+			if(progress < 0){
+				printf("failure on day %d\n", day);
+				break;
+			}
+
+			if(climb >= loss){
+				climb -= loss;
+			}
+
+			else{
+				climb = 0;
+			}
+			day++;
+		}
+	}
+}
